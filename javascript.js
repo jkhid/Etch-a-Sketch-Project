541b1e44afdd3040e48a7622b1e3d0579ee0b0ef
Jamal Khidir <116044175+jkhid@users.noreply.github.com>
2023-05-18 08:48:49 -0700

    Create rough draft of createGrid function

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// Create a function that produces 16x16 grid of divs 
+const container = document.getElementById("container");
+
+function createGrid(x) {
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < x; j++){
+            const cell = document.createElement("div");
+
+            container.appendChild(cell)
+            cell.classList.add("cells")
+        }
+    }
+}
+createGrid(16);
+// Create a hover effect that changes the grids color 
+// Add a button that will ask the user the amount of squares they want 

b1edb2ea85042db19ca1a00d7bb5e0dc84404d97
Jamal Khidir <116044175+jkhid@users.noreply.github.com>
2023-05-20 12:35:16 -0700

    Add change grid button functionality

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@
 const container = document.getElementById("container");
 
 function createGrid(x) {
+    container.innerHTML = "";
     container.style.display = "grid";
     container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
     for (let i = 0; i < x; i++) {
@@ -13,24 +14,45 @@ function createGrid(x) {
         }
     }
 }
-createGrid(24);
+
+// initial grid 
+createGrid(16);
 
 // Create a hover effect that changes the grids color 
+function hoverEffect() {
 const cells = document.querySelectorAll(".cells");
-
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {
         cell.classList.add("hovered");
     })
 })
+}
+
+hoverEffect();
 
 // Functionality for Clear button
 const clearBtn = document.querySelector(".clear");
 
 clearBtn.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".cells");
+    clearBtn.addEventListener("click", () => {
     cells.forEach(cell => {
         cell.classList.remove("hovered");
     })
 })
+})
 
 // Functionality for Change Grid button 
+const gridNum = document.querySelector(".gridNum");
+
+gridNum.addEventListener("click", () => {
+    const gridSizeInput = prompt("Enter a grid size number!");
+    const gridSize = parseInt(gridSizeInput);
+
+    if (gridSize > 0) {
+        createGrid(gridSize);
+        hoverEffect();
+    } else {
+        alert("Please enter a valid number");
+    }
+})
